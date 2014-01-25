85ca3efc7de5f3522371c9b5815957eafc3ebca3
John Cupitt <jcupitt@gmail.com>
2014-01-25 17:53:10 +0000

    update vips.rb to 7.38.1
    update vips to latest stable ... summary of new stuff:
    
    http://libvips.blogspot.co.uk/2014/01/new-stable-version-738.html

--- vips.rb
+++ vips.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Vips < Formula
   homepage 'http://www.vips.ecs.soton.ac.uk/'
-  url 'http://www.vips.ecs.soton.ac.uk/supported/7.36/vips-7.36.5.tar.gz'
-  sha1 '6911066ce7925666dce2aaa676e006afb79b9a25'
+  url 'http://www.vips.ecs.soton.ac.uk/supported/7.38/vips-7.38.1.tar.gz'
+  sha1 'd81538b0bc848f2c8b3180ec931b4dd96eefd42a'
 
   option 'without-check', 'Disable build time checks (not recommended)'
 
