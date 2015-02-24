5216dd804025b1e8d153f6f6de12d71af3eb09dc
John Cupitt <jcupitt@gmail.com>
2015-02-24 09:01:15 +0000

    fix reported problems
    remove "formula" line
    
    add an check option ... disabled, by default since "make check" takes
    over five minutes

--- nip2.rb
+++ nip2.rb
@@ -1,9 +1,9 @@
-require "formula"
-
 class Nip2 < Formula
   homepage "http://www.vips.ecs.soton.ac.uk/"
   url "http://www.vips.ecs.soton.ac.uk/supported/7.42/nip2-7.42.1.tar.gz"
   sha1 "46e48e003621c5c63a5bf0e126f7ace0c0b54746"
+
+  option "with-check", "Enable build-time checking"
 
   depends_on "pkg-config" => :build
   depends_on "gtk+"
