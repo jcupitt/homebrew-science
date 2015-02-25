d684838f0aa8b2925aebcca7c33995f6d7ea805f
John Cupitt <jcupitt@gmail.com>
2015-02-25 11:52:37 +0000

    make ~/Library
    when first run, nip2 tries to create ~/Library/nip2-x.y.z
    to hold temp files ... ~/Library is not present in homebrew's
    private test home directory, make it
    
    with this change, "brew test --verbose nip2" passes

--- nip2.rb
+++ nip2.rb
@@ -29,6 +29,12 @@ class Nip2 < Formula
   end
 
   test do
+    # when first run, nip2 tries to create ~/Library/nip2-x.y.z
+    # to hold temp files ... ~/Library is not present in homebrew's
+    # private test home directory
+    require 'fileutils'
+    FileUtils.mkdir_p ENV['HOME'] + '/Library'
+
     system "#{bin}/nip2", "--benchmark" 
   end
 end
